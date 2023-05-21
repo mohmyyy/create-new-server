38190704e230b477a91930be615a34c1c3c388ca
mohmyyy <iammohmy@gmail.com>
2023-05-21 09:40:23 +0530

    learning Request / Response

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  // console.log(req);
+  const url = req.url;
+  if (url === "/node") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title> Learning Node</title></head>");
+    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/home") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>home</title></head>");
+    res.write("<body><h1>Welcome home</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title> Learning Node</title></head>");
+    res.write("<body><h1>Welcome to About Us page</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title> Learning Node</title></head>");
+  res.write("<body><h1>Welcome</h1></body>");
+  res.write("</html>");
+  return res.end();
 });
 
-server.listen(4000);
+server.listen(3000);
